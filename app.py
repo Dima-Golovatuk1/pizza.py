5af35247f91ca9cbac87608ea9bec5b8c4917087
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-06-13 12:44:34 +0300

    update

--- app.py
+++ app.py
@@ -2,85 +2,9 @@ from flask import Flask
 from flask import render_template
 import random
 import datetime
+import sqlite3
 
 app = Flask(__name__)
-
-
-current_day = datetime.datetime.now().weekday()
-
-
-pizzas = [
-    {
-        "name": "Маргарита",
-        "price": "150 грн.",
-        "composition": "томатний соус, моцарелла, базилік.",
-        "Diameter": "30 см.",
-        "Weight": "550 грам.",
-        "number": 1
-    },
-    {
-        "name": "Пепероні",
-        "price": "170 грн.",
-        "composition": "томатний соус, моцарелла, пепероні.",
-        "Diameter": "35 см.",
-        "Weight": "600 грам.",
-        "number": 1
-    },
-    {
-        "name": "Гавайська",
-        "price": "180 грн.",
-        "composition": "томатний соус, моцарелла, шинка, ананаси.",
-        "Diameter": "35 см.",
-        "Weight": "620 грам.",
-        "number": 1
-    },
-    {
-        "name": "Чотири сири",
-        "price": "190 грн.",
-        "composition": "томатний соус, моцарелла, пармезан, горгонзола, дор-блю.",
-        "Diameter": "35 см.",
-        "Weight": "650 грам.",
-        "number": 1
-    },
-    {
-        "name": "М'ясна",
-        "price": "200 грн.",
-        "composition": "томатний соус, моцарелла, салямі, бекон, шинка.",
-        "Diameter": "40 см.",
-        "Weight": "700 грам.",
-        "number": 1
-    },
-    {
-        "name": "Вегетаріанська",
-        "price": "180 грн.",
-        "composition": "томатний соус, моцарелла, перець, гриби, цибуля, оливки.",
-        "Diameter": "35 см.",
-        "Weight": "620 грам.",
-        "number": 1
-    }
-]
-
-
-if current_day == 5:
-    pizzas.append({        
-        "name": "Суботня",
-        "price": "200 грн.",
-        "composition": "томатний соус, моцарелла, перець, гриби, папероні, чеддер.",
-        "Diameter": "37 см.",
-        "Weight": "700 грам.",
-        "number": 1})
-elif current_day == 6:
-        for i in pizzas:
-            if i ["name"] == "М'ясна":
-                i ["price"] = "170 грн."
-        for i in pizzas:
-            if i ["name"] == "Чотири сири":
-                i ["price"] = "175 грн."
-elif current_day == 0:
-        for i in pizzas:
-            if i ["name"] == "Маргарита":
-                i ["price"] = "140 грн."
-
 
 
 
@@ -89,9 +13,13 @@ def hellp_world():
     return render_template("base.html")
 
 
-@app.route('/menu')
+@app.route('/menu', methods=["GET", "POST"])
 def name():
-    return render_template("menu.html", date=pizzas)
+    with sqlite3.connect("menu.db") as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM MENU")
+        datamenu = cursor.fetchall()
+    return render_template("menu.html", data=datamenu)
 
 
 @app.route('/contact')
